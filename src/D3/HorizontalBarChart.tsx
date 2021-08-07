f1565051979da5dfbaa648b8b935283c8a8c4734
Abdelnacerr <abdelnacerr@gmail.com>
2021-08-07 21:30:57 +1000

    wrong way horizontal bar

--- src/D3/HorizontalBarChart.tsx
+++ src/D3/HorizontalBarChart.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import * as d3 from 'd3'
 import data from './Data'
 
@@ -11,26 +11,24 @@ const HorizontalBarChart = () => {
 		undefined
 	>>(null)
 
-	//svg container width/height
-	const svgWidth = 500,
-		svgHeight = 300
+	// const data = dataset.sort(function (a, b) {
+	// 	return d3.ascending(a.value, b.value)
+	// })
 
-	const barWidth = svgWidth / data.length //width of each bar
+	const svgWidth = 750,
+		svgHeight = 750
 
 	const maxValue = d3.max(data, (d) => d.value)
 
-	const y = d3.scaleLinear().domain([0, maxValue!]).range([0, svgHeight])
+	const x = d3.scaleLinear().domain([0, maxValue!]).range([0, svgHeight])
 
-	const x = d3
+	const y = d3
 		.scaleBand()
 		.domain(data.map((d) => d.name))
 		.range([0, svgWidth])
 		.paddingInner(0.05)
 
 	const colorScale = d3.scaleOrdinal(d3.schemeCategory10)
-
-	// const yAxis = d3.axisLeft(y).ticks(3)
-	// const xAxis = d3.axisBottom(x)
 
 	useEffect(() => {
 		if (!selection) {
@@ -43,24 +41,24 @@ const HorizontalBarChart = () => {
 				.data(data)
 				.enter()
 				.append('rect')
-				.attr('width', x.bandwidth)
-				.attr('height', (d) => y(d.value))
-				.attr('x', (d) => x(d.name)!)
-				.attr('y', (d) => svgHeight - y(d.value))
+				.attr('height', y.bandwidth)
+				.attr('width', (d) => x(d.value))
+				.attr('y', (d) => y(d.name)!)
+				.attr('x', (d) => svgHeight - x(d.value))
 				.attr('fill', (d) => colorScale(d.name))
 
-			const text = svg
-				.selectAll('text')
-				.data(data) //data in waiting state
-				.enter()
-				.append('text') //takes a string or a function as a param
-				.text((d) => d.name)
-				.attr('y', (d) => svgHeight - d.value - 2) //subtract extra 2 pixels
-				.attr('x', (d, i) => barWidth * i)
-				.style('fill', 'blue')
+			// const text = svg
+			// 	.selectAll('text')
+			// 	.data(data) //data in waiting state
+			// 	.enter()
+			// 	.append('text') //takes a string or a function as a param
+			// 	.text((d) => d.name)
+			// 	.attr('y', (d) => svgHeight - d.value - 2) //subtract extra 2 pixels
+			// 	.attr('x', (d, i) => barWidth * i)
+			// 	.style('fill', 'blue')
 		}
 	}, [selection, colorScale, svgHeight, svgWidth, x, y])
-	// console.log(data[0].value)
+
 	return (
 		<div>
 			<h1>Bar Chart</h1>
